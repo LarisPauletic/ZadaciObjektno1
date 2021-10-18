0c34fa140c8c902dcb7c55c24eae22a74195a569
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-10-18 09:24:32 +0200

    Add files via upload

---
+++ ZadaciObjektno1/ZadaciObjektno1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciObjektno1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport Nogomet = new Sport("Nogomet", true, 11);
+            Sport Vaterpolo = new Sport("Vaterpolo", true, 7);
+            Sport Curling = new Sport("Curling", false,  5);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ZadaciObjektno1/ZadaciObjektno1/Sport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciObjektno1
+{
+    class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+
+        public Sport(string s)     
+        {   
+            Naziv = s;     
+        
+        } 
+        public Sport(string s, bool b)     
+        {   
+            Naziv = s;         
+            IgraLiSeLoptom = b;     
+        } 
+        public Sport(string s, bool b, byte n)     
+        {   
+            Naziv = s;         
+            IgraLiSeLoptom = b;         
+            BrojIgraca = n;     
+        } 
+    
+    }
+    
+}
